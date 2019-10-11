9b2e0c387e7832868d528a6015d0d608f131177f
costagavras <cgavras@yahoo.ca>
2019-10-10 23:14:32 -0400

    login form basic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -46,7 +46,8 @@ import { BalanceComponent } from './balance/balance.component';
     BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+// import { Subscription } from 'rxjs';
+
+// import { AuthService } from '../auth.service';
+// import { UIService } from '../../shared/ui.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+
+export class LoginComponent implements OnInit, OnDestroy {
+  loginForm: FormGroup;
+  // isLoading = false;
+  // private loadingSubs: Subscription;
 
   constructor() { }
+  // private authService: AuthService,
+  //             private uiService: UIService)
 
   ngOnInit() {
+    // this.loadingSubs = this.uiService.loadingStateChanged
+    //   .subscribe(isLoading => {
+    //     this.isLoading = isLoading;
+    //   });
+    this.loginForm = new FormGroup ({
+      email: new FormControl('', {validators: [Validators.required, Validators.email]}),
+      password: new FormControl('', {validators: [Validators.required]}),
+    });
+  }
+
+  onSubmit() {
+    // this.authService.login({
+    //   email: this.loginForm.value.email,
+    //   password: this.loginForm.value.password
+    // });
+
+  }
+
+  // getters for validation control
+  get email() { return this.loginForm.get('email'); }
+
+  get password() { return this.loginForm.get('password'); }
+
+  ngOnDestroy() {
+    // if (this.loadingSubs) {
+    //   this.loadingSubs.unsubscribe();
+    // }
   }
 
 }
